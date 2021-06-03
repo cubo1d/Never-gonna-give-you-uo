d3abf9090d90cec70b9762d142d37476a1691ad3
cubo1d <85317741+cubo1d@users.noreply.github.com>
2021-06-03 22:54:53 +0100

    Add project files.

---
+++ raj/ur mom.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace raj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Hi.exe";
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WindowHeight = 2;
+
+            Console.WriteLine("hi");
+
+            Console.ReadKey();
+        }
+    }
+}
